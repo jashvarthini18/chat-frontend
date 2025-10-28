0daef8aaff08f95a1543627b0c5efb2dc3b3f8c9
Jashvarthini Rajesh <jashvarthinirajesh@gmail.com>
2025-10-28 11:06:04 +0530

    Fix: base url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://chat-backend-91gt.onrender.com/api"; // <-- your Render backend
+    : "https://chat-backend-opco.onrender.com/api"; // <-- your Render backend
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
