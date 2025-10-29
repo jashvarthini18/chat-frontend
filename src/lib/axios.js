eb22546e768d822fd529c48d25f30966a16e65c2
Jashvarthini Rajesh <jashvarthinirajesh@gmail.com>
2025-10-29 11:45:33 +0530

    axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,21 +1,11 @@
-// import axios from "axios";
-
-// const BASE_URL =
-//   import.meta.env.MODE === "development"
-//     ? "http://localhost:5001/api"
-//     : "https://chat-backend-opco.onrender.com/api"; // <-- your Render backend
-
-// export const axiosInstance = axios.create({
-//   baseURL: BASE_URL,
-//   withCredentials: true,
-// });
-
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://chat-backend-opco.onrender.com/api"; // <-- Your Render backend
+
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "https://chat-backend-opco.onrender.com/api", // ✅ Full backend URL
-  withCredentials: true, // ✅ This is crucial
+  baseURL: BASE_URL,
+  withCredentials: true, // ✅ Must be true to send cookies (JWT)
 });
