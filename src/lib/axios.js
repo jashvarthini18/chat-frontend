a81bb951f96dcea12f4c82d93b3060f83b1765f5
Jashvarthini Rajesh <jashvarthinirajesh@gmail.com>
2025-11-04 20:06:55 +0530

    Fix: Rendering connection

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,7 +5,13 @@ const BASE_URL =
     ? "http://localhost:5001/api"
     : "https://chat-backend-opco.onrender.com/api"; // <-- your Render backend
 
+// export const axiosInstance = axios.create({
+//   baseURL: "https://chat-backend-opco.onrender.com/api",
+//   withCredentials: true,
+// });
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-backend-opco.onrender.com/api",
-  withCredentials: true,
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://chat-backend-opco.onrender.com/api",
+  withCredentials: true
 });
